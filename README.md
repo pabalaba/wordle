44dc16394080100c4d7faa9d78ff52afc2c4f747
Daniele Barbaro <barbaro.daniele@gmail.com>
2022-04-13 09:58:08 +0200

    Scheletro di aiuto, Readme ed esempio

---
+++ README.md
@@ -0,0 +1,24 @@
+# WORDLE console Game
+
+Implementare il famosissimo gioco [WORDLE](https://www.nytimes.com/games/wordle/index.html).
+
+Ogni tentativo deve avere 4 lettere valide, il colore della lettera deve cambiare secondo le seguenti regole:
+
+ - *VERDE*: la lettera è contenuta nella soluzione nella posizione giusta
+ - *GIALLO*: la lettera è contenuta nella soluzione ma NON nella posizione giusta
+ - *GRIGIO*: la lettera NON è contenuta nella soluzione
+
+La soluzione è la parola CODE come descritto a riga 27
+
+### Per installare il progetto:
+```javascript
+npm install
+```
+
+### Per giocare:
+```javascript
+node index.mjs
+```
+
+#### Esempio di Giocata:
+<p align="center"><img src="/media/wordle_sample_result.png" alt="Sample game output"></p>
--- index.mjs
+++ index.mjs
@@ -1,24 +1,91 @@
+/**
+ * Implementare WORDLE. [https://www.nytimes.com/games/wordle/index.html]
+ *   Ogni tentativo deve avere 4 lettere valide.
+ *
+ *   Il colore della lettera deve cambiare secondo le seguenti regole:
+ *
+ *   VERDE: la lettera è contenuta nella soluzione nella posizione giusta
+ *   GIALLO: la lettera è contenuta nella soluzione ma NON nella posizione giusta
+ *   GRIGIO: la lettera non è contenuta nella soluzione
+ *
+ * */
+
 import chalk from 'chalk';
 import readline from 'readline';
 
-console.log(chalk.green('Test verde!'));
-console.log(chalk.yellow('Test gialla!'));
-console.log(chalk.gray('Test Grigia!'));
-
-//https://nodejs.org/api/readline.html#readlinecreateinterfaceoptions
+// https://nodejs.org/api/readline.html#readlinecreateinterfaceoptions
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 })
 
-rl.question('Inserisci una parola ', (answer) => {
-    console.log(`la parola inserita è: ${answer}`);
-    rl.close();
-});
+let attempt = 0 // contatore dei tentativi
+let maxGameAttempt = 6; // abbiamo 6 tentativi per risolvere una parola
+let wordGameLength = 4; // la parola deve esser lunga al max 4
 
-// abbiamo 6 tentativi per risolvere una parola
+// Soluzione statica a fine didattico.
+const solution = 'CODE';
 
-// la parola deve essere lunga 4
+// funzione di verifica
+const wordChecker = function (word, solution) {
+    let success = [];
+    let result = [];
+
+    // ATTENZIONE ALLE PAROLE maiuscole/minuscole.
+
+    // controllo lettera per lettera in parola
+    for (let index in word) {
+        // VERDE: la lettera è contenuta nella parola nella posizione giusta
+        // GIALLO: la lettera è contenuta nella parola ma NON nella posizione giusta
+        // GRIGIO: la lettera non è contenuta nella parola
+    }
+
+    // ritorno un risultato composto dalle lettere colorate e da un success booleano
+    // per fare uscire dal gioco se l'utente indovina la parola
+    return {
+        'data': '',
+        'success': true
+    }
+}
+
+// funzione di Gioco
+const game = function (attempt, max) {
+    // https://nodejs.org/api/readline.html#rlquestionquery-options-callback
+    rl.question(chalk.blue(`Inserisci una parola di ${wordGameLength} caratteri: `), function (answer) {
+
+        // usiamo una safe word per uscire dal ciclo
+        if (answer === 'exit') {
+            return rl.close();
+        }
+
+        // controlla di aver inserito esattamente wordGameLength caratteri altrimenti dai un errore
+        // if (condition) {
+        //      console.log('\n', chalk.red(`Devi inserire ${wordGameLength} caratteri.`));
+        // }
+
+
+        console.log('\n', `Tentativo ${attempt} di ${max}`);
+        ++attempt;
+
+
+        // if (condition) {
+        //     console.log(chalk.green(' >> HAI VINTO << '));
+        //     return rl.close();
+        // }
+
+        // if (condition) {
+        //     console.log('\n', chalk.red(`Spiacente hai terminato i ${counter} tentativi. La soluzione era ${solution}`), '\n');
+        //     return rl.close();
+        // }
+
+        // Richiamo la funzione fino a esaurimento dei tentativi
+        game(attempt, maxGameAttempt);
+    });
+}
+
+game(attempt, maxGameAttempt);
+
+
 
 
 
